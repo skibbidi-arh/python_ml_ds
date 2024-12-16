1d0adb0d3efa4bbae5eab2232dfd84dd1fe47b36
Arham Apon <arhamaponutsho@gmail.com>
2024-12-17 03:34:20 +0600

    6

---
+++ numpy/1.py
@@ -0,0 +1,8 @@
+import numpy as np
+a= np.array([1,2,3])
+print(a)
+b=np.array([[1,2,3],[4,5,6]]) # 2 dimensional
+print(b)
+print(a.ndim) #prints number of dimensions
+print(b.shape) #prints like (i,j) of a vector
+print(b.size)#total number of elements
